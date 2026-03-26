880f912db674c35c35920a34c79708109ec3a9db
tkasymov <kasymofftm@icloud.com>
2026-03-26 16:49:36 +0300

    commit directory

--- 2-ex9.py
+++ 2-ex9.py
@@ -6,11 +6,18 @@ URL_CHECK_COOKIE = "https://playground.learnqa.ru/ajax/api/check_auth_cookie"
 LOGIN = "super_admin"
 
 passwords = [
-    "123456", "password", "123456789", "12345", "12345678",
-    "qwerty", "111111", "1234567", "iloveyou", "adrian",
-    "abc123", "dragon", "sunshine", "master", "hello",
-    "princess", "monkey", "letmein", "mustang", "696969",
-    "shadow", "michael", "asdf", "2000", "jordan"
+    "1234", "123123", "12345", "123456", "1234567", "12345678",
+    "123456789", "1234567890", "1q2w3e4r", "1qaz2wsx", "654321",
+    "666666", "696969", "7777777", "888888", "aa123456",
+    "abc123", "access", "adobe123", "admin", "ashley",
+    "azerty", "bailey", "baseball", "batman", "charlie",
+    "donald", "dragon", "flower", "football", "freedom",
+    "hottie", "iloveyou", "letmein", "lovely", "login",
+    "loveme", "master", "michael", "monkey", "mustang",
+    "ninja", "passw0rd", "password", "password1", "photoshop",
+    "princess", "qazwsx", "qwerty", "qwerty123", "qwertyuiop",
+    "shadow", "solo", "starwars", "sunshine", "superman",
+    "trustno1", "whatever", "welcome", "zaq1zaq1"
 ]
 
 print("Подбор пароля для логина:", LOGIN)
